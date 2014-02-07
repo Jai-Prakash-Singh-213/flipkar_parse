aa6fc6ec28d4ec769c3f7dc3a669acbcb6d10f95
Jai-Prakash-Singh <jp213@ymail.com>
2014-02-08 00:54:30 +0530

    08:02:14 12:54 am

---
+++ allinone/allinone/spiders/code2_allinone.py
@@ -0,0 +1,89 @@
+import code1_allinone
+import multiprocessing
+import os
+import time 
+from Queue import Queue
+from threading import Thread
+import time
+import urll_proxy
+from bs4 import BeautifulSoup
+
+num_fetch_threads = 20
+enclosure_queue = Queue()
+
+
+
+def mystrip(x):
+    return x.strip()
+
+
+def main3(i, q):
+    while True:
+        directory, link = q.get()
+
+        page = urll_proxy.main(link)
+        html = page.read()
+        soup = BeautifulSoup(html)
+        page.close()
+
+	if soup.find("ul", attrs={"id":"brand"}):
+	    filename = directory + "/" + extract_brand_from_it.txt
+	    f = open(filename, "a+")
+	    print >>f, link
+	    f.close()
+
+	if 
+
+
+
+
+
+
+def main2(dte, menu, links_list):
+    directory = dte + "/" +menu
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+     
+    for i in range(num_fetch_threads):
+        worker = Thread(target=main3, args=(i, enclosure_queue,))
+	#worker.setDaemon(True)
+	worker.start()
+
+    for link in links_list:
+        enclosure_queue.put((directory, link))
+
+    print '*** Main thread waiting'
+    enclosure_queue.join()
+    print '*** Done'
+    
+        
+
+
+
+def main():
+    #dict_menu_links = code1_allinone.main()
+    
+    #f = open("dict_menu_links", "a+")
+    #print >>f, dict_menu_links
+    #f.close()
+
+    dat = time.strftime("%d%m%Y")
+
+    f = open("dict_menu_links")
+    dict_menu_links = eval(f.read().strip())
+    f.close()
+ 
+    for menu, links_list in dict_menu_links.items():
+        p = multiprocessing.Process(target=main2, args=(dte, menu, links_list))
+	p.start()
+        
+	    
+        
+
+    
+
+
+if __name__=="__main__":
+    main()
+    
